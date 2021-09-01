2ca00506120ca6c5478f305921788080934d6860
Benneth <amaben202phantom@gmail.com>
2021-09-01 12:56:19 +0100

    Added new products

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
 // module.exports = ({ env }) => ({
-//   defaultConnection: 'default',
+//   defaultConnection: "default",
 //   connections: {
 //     default: {
-//       connector: 'mongoose',
+//       connector: "mongoose",
 //       settings: {
-//         host: env('DATABASE_HOST', 'cluster0.oklwv.mongodb.net'),
-//         srv: env.bool('DATABASE_SRV', true),
-//         port: env.int('DATABASE_PORT', 27017),
-//         database: env('DATABASE_NAME', 'bejamas'),
-//         username: env('DATABASE_USERNAME', 'Admin'),
-//         password: env('DATABASE_PASSWORD', 'ugonna2018'),
+//         host: env("DATABASE_HOST", "cluster0.oklwv.mongodb.net"),
+//         srv: env.bool("DATABASE_SRV", true),
+//         port: env.int("DATABASE_PORT", 27017),
+//         database: env("DATABASE_NAME", "bejamas"),
+//         username: env("DATABASE_USERNAME", "Admin"),
+//         password: env("DATABASE_PASSWORD", "ugonna2018"),
 //       },
 //       options: {
-//         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-//         ssl: env.bool('DATABASE_SSL', true),
+//         authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+//         ssl: env.bool("DATABASE_SSL", true),
 //       },
 //     },
 //   },
@@ -25,7 +25,6 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        // uri: env("DATABASE_URI"),
         host: env("DATABASE_HOST"),
         srv: env.bool("DATABASE_SRV", true),
         port: env.int("DATABASE_PORT", 27017),
